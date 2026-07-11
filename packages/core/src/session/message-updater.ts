a6078140d0bb17f2fc2f40ff824f5f38fe0e15de
grappeggia <marcosgrappeggia@gmail.com>
2026-07-11 09:14:21 -0700

    fix(session): harden V2 task recovery

--- packages/core/src/session/message-updater.ts
+++ packages/core/src/session/message-updater.ts
@@ -100,6 +100,7 @@ export function update(adapter: Adapter, event: SessionEvent.Event) {
 
   return Effect.gen(function* () {
     yield* SessionEvent.All.match(event, {
+      "session.next.created": () => Effect.void,
       "session.next.agent.switched": (event) => {
         return adapter.appendMessage(
           new SessionMessage.AgentSwitched({
